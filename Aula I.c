aa579b08f70e2901c3f441432a84c0799f79950e
joaojesus666 <82557949+joaojesus666@users.noreply.github.com>
2022-05-27 20:15:59 -0300

    Create Aula I.c

---
+++ Aula I.c
@@ -0,0 +1,24 @@
+/*
+Comentarios as linhas que estiverem entre.
+*/
+ 
+//Comenta a linha.
+ 
+//Declaração de biblioteca.
+#include<stdio.h>//Biblioteca principal.
+ 
+//Função principal.
+int main(){
+   //Declaração de váriavel.
+   int a;
+   
+   //Incialização de váriavel.
+   a = 2;
+   
+   //Impressão.
+   printf("Olá UFMT!\n");
+   
+   //Final da função principal.
+   return 0;
+}
+
